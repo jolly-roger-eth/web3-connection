7240202992fc48dabe7667d01925ab730326a07f
wighawag <wighawag@gmail.com>
2024-01-30 11:26:20 +0000

    latest deps

--- src/utils/index.d.ts
+++ src/utils/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="svelte" />
 import type { Readable } from 'svelte/store';
 export declare function waitFor<T>(connection: Readable<T>, object: Partial<T>): Promise<any>;
 export declare function wait(numSeconds: number): Promise<unknown>;
